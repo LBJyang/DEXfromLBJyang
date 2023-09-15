484136a863c8ee3610dcc198b6f30799a90d23ca
lbjyangoffice <yangfan6262008@gmail.com>
2023-09-15 11:03:16 +0800

    update poolcontract

--- src/Uniswapv3pool.sol
+++ src/Uniswapv3pool.sol
@@ -1,6 +1,40 @@
-//SPDX-License-Identifier:MIT
+//SPDX-License-Identifier:BUSL-1.1
 pragma solidity ^0.8.14;
 
-contract Uniswapv3pool{
-    
+import "../lib/Tick.sol";
+import "../lib/Position.sol";
+
+contract Uniswapv3pool {
+    mapping(int24 => Tick.Info) public ticks;
+    mapping(bytes32 => Position.Info) public positions;
+
+    using Tick for mapping(int24 => Tick.Info);
+    using Position for mapping(bytes32 => Position.Info);
+    using Position for Position.Info;
+
+    uint128 public liquidity;
+
+    int24 internal constant MIN_TICK = -887272;
+    int24 internal constant MAX_TICK = -MIN_TICK;
+
+    address public immutable token0;
+    address public immutable token1;
+
+    struct Slot0 {
+        uint160 sqrtPriceX96;
+        int24 tick;
+    }
+
+    Slot0 public slot0;
+
+    constructor(
+        address token0_,
+        address token1_,
+        uint160 sqrtPriceX96_,
+        int24 tick_
+    ) {
+        token0 = token0_;
+        token1 = token1_;
+        slot0 = Slot0({sqrtPriceX96:sqrtPriceX96_,tick:tick_});
+    }
 }
